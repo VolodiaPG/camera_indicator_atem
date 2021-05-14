e03677807a4d6e8256a3bbece8e3eae7c0236b47
Volodia PAROL-GUARINO <volodia.parolguarino@gmail.com>
2021-05-14 18:52:30 +0200

    [Back] Add removed atem script

---
+++ back/atem.ps1
@@ -0,0 +1,17 @@
+add-type -path './8_5_3/SwitcherLib.dll'
+$Global:atem = New-Object SwitcherLib.Switcher("192.168.0.40")
+$atem.Connect()
+$atem.GetSwitcherLibVersion()
+
+$ME = $atem.GetMEs()
+$uri = 'http://localhost:8000/api/atem'
+$last_data = ""
+while ($true)
+{
+    $data = "{`"air`": $($me[0].Program), `"preview`": $($me[0].Preview)}"
+    if ($data -ne $last_data){
+        Invoke-WebRequest -Uri $uri -Body $data -ContentType 'application/json' -Method POST -UseBasicParsing
+        $last_data = $data
+    }
+    Start-Sleep -Milliseconds 30  
+} 
